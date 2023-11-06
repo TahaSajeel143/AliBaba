d0053a2a0a422a7b80eb84baceda6b0f408bac20
TahaSajeel143 <itxtaha227@gmail.com>
2023-11-06 11:54:12 -0800

    done

--- Controllers/AliBaba.js
+++ Controllers/AliBaba.js
@@ -4,7 +4,7 @@ import categorySchema from '../Models/categorySchema';
 
 const addproduct = async (req, res) => {
     try {
-        const { title ,description, reviews, buyers,  priceOfPieces, benefits, color, customization, leadTime, category } = req.body;
+        const { title ,description, reviews, buyers,  priceOfPieces, benefits, color, customization, leadTime, category, images } = req.body;
 
         const newProduct = new productSchema({
             title,
@@ -17,6 +17,7 @@ const addproduct = async (req, res) => {
             customization,
             leadTime,
             category,
+            images,
         });
 
         const savedProduct = await newProduct.save(); // Use await here
@@ -73,31 +74,21 @@ const addcategory = async (req, res) => {
 
 
   const getAllProducts = (req, res) => {
-    // Assuming status is defined and propertySchema is correctly defined
-    // Make sure you've established a database connection using Mongoose
-  
-    // Use propertySchema to find properties in the database
     productSchema.find({})
-      .then(product => {
-        if (product.length > 0) {
-          // Send the properties as a response if found
-          res.status(status.OK).send(product);
-        } else {
-          // Send a "Not Found" response if no properties are found
-          res.status(status.NOT_FOUND).send({
-            Message: 'No property found.',
-          });
-        }
+      .then(products => {
+        res.status(status.OK).json({
+          products
+        });
       })
       .catch(err => {
-        // Handle any errors that occur during the database query
         console.error(err);
-        res.status(status.INTERNAL_SERVER_ERROR).send({
+        res.status(status.INTERNAL_SERVER_ERROR).json({
           Message: 'Internal server error',
           Error: err.message,
         });
       });
   };
+  
 
 
 
